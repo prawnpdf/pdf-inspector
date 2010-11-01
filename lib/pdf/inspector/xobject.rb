907eda0d9a6de02b4c09d1794988253f91db8ebf
jonathangreenberg <greenberg@entryway.net>
2010-10-31 22:56:39 -0400

    added support for storing xobject streams

--- lib/pdf/inspector/xobject.rb
+++ lib/pdf/inspector/xobject.rb
@@ -1,18 +1,24 @@
 module PDF
   class Inspector
     class XObject < Inspector
-      attr_accessor :page_xobjects
+      attr_accessor :page_xobjects, :xobject_streams
 
       def initialize
         @page_xobjects = []
+        @xobject_streams = {}
       end
 
       def resource_xobject(*params)
         @page_xobjects.last << params.first
+        store_streams(params.first, params.last)
       end
 
       def begin_page(*params)
         @page_xobjects << [] 
+      end
+      
+      def store_streams(resource_name, stream)
+        @xobject_streams[resource_name] = stream
       end
     end
   end
